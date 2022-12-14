402559e5705719c9f2a55e0d7b52f1d121d86b44
Peter Evance <59042622+peter-evance@users.noreply.github.com>
2022-12-14 23:47:13 +0300

    Progress

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet  - This prints the alphabet a - z
+ */
+void print_alphabet(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
